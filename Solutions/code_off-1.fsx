ea32028d77258fd8fedca1daf36acbb3a5a4c147
anaude1 <anaude@silica.net>
2016-03-14 14:40:19 +0200

    Minor code clean-up of solution #1

--- Solutions/code_off-1.fsx
+++ Solutions/code_off-1.fsx
@@ -4,8 +4,7 @@ open System.IO
 let inputPath = Path.Combine(__SOURCE_DIRECTORY__ , @"..\code_off-1\code_off-1.in")
 let outputPath = Path.Combine(__SOURCE_DIRECTORY__ , @"..\code_off-1\code_off-1.out")
 
-let charValue c =
-    match c with
+let charValue = function
     | uc when (int)uc - (int)'A' < 26 -> (int)uc - (int)'A' + 1
     | lc when (int)lc - (int)'a' < 26 -> (int)lc - (int)'a'
     | _ -> 0
@@ -19,7 +18,7 @@ let duplicateRows rows input =
     |> Seq.filter (fun a -> a <> input && (rowValue a = rowValue input))
 
 let isPalindrome (a : string) =
-    new String(a.ToCharArray() |> Array.rev) = a
+    a = new String(a.ToCharArray() |> Array.rev)
 
 let input = 
     File.ReadAllLines inputPath
@@ -28,7 +27,12 @@ let input =
 let answer = 
     input
     // Problem statement doesn't include how a "list of matching rows" should be represented - opting for ; as separator...
-    |> Seq.map (fun a -> [a; isPalindrome a |> string ;String.Join(";", duplicateRows input a)])
+    |> Seq.map (fun a -> 
+        [
+            a;
+            isPalindrome a |> string;
+            String.Join(";", duplicateRows input a)
+        ])
     |> Seq.fold (fun state elem -> Seq.append state elem) Seq.empty
 
 File.WriteAllLines(outputPath, answer)
